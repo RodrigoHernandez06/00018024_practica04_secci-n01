3b18dc4552d6e06e69f48f511f6b0a15071f2314
RodrigoHernandez06 <00018024@uca.edu.sv>
2025-09-23 20:18:24 -0600

    💄: Cambiando diseño UI

--- EjercicioFinal/script.js
+++ EjercicioFinal/script.js
@@ -1,24 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const planes = [
-        {id: 1, tarea: 'Atender al cliente'},
-        {id: 2, tarea: 'Visitar la agencia fuera de la ciudad'},
-        {id: 3, tarea: 'Llevar a mi hija al ballet'},
-        {id: 4, tarea: 'Priorizar entregas de desarrollo'},
-        {id: 5, tarea: 'Atender problemas remotos'},
-        {id: 6, tarea: 'Lo que siga mi señora'},
-    ]
+function mostrarRecordatorio(dia) {
+  let mensaje = "";
 
-    const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+  switch (dia.toLowerCase()) {
+    case "lunes":
+      mensaje = "Atender a un cliente especifico.";
+      break;
+    case "martes":
+      mensaje = "Visitar un agencia fuera de la ciudad.";
+      break;
+    case "miércoles":
+    mensaje = "Llevar a mi hija al ballet.";
+      break;
+    case "jueves":
+      mensaje = "Priorizar entregas de desarrollo.";
+      break;
+    case "viernes":
+      mensaje = "Atender problemas de manera remota.";
+      break;
+    case "sábado":
+      mensaje = "Hoy debes hacer lo que tu esposa quiera.";
+      break;
+    case "domingo":
+      mensaje = "No tienes actividades programadas";
+      break;
+  }
 
-    const contenedorBotones = document.getElementById('recordatorios');
-
-   function renderizarBotones() {
-        const boton = document.createElement('button');
-        const texto = document.createElement('p')
-        contenedorBotones.appendChild(boton);
-        contenedorBotones.appendChild(texto);
-   }
-
-   dias.forEach(renderizarBotones)
-
-}); 
+  document.getElementById("resultado").innerText = mensaje;
+}
