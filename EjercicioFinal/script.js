95fdcece7bb852e34a7cf92b962742d1d600c2da
RodrigoHernandez06 <00018024@uca.edu.sv>
2025-09-23 19:52:18 -0600

    :wq!

--- EjercicioFinal/script.js
+++ EjercicioFinal/script.js
@@ -1,13 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.assert(true);
-    console.clear();
-    console.dir(window);
-    console.dirxml(document.body);
+    const planes = [
+        {id: 1, tarea: 'Atender al cliente'},
+        {id: 2, tarea: 'Visitar la agencia fuera de la ciudad'},
+        {id: 3, tarea: 'Llevar a mi hija al ballet'},
+        {id: 4, tarea: 'Priorizar entregas de desarrollo'},
+        {id: 5, tarea: 'Atender problemas remotos'},
+        {id: 6, tarea: 'Lo que siga mi señora'},
+    ]
 
-    const marvel = ['Spider Man', 'Iron Man', 'Capitan Anmerica', 'Wolverine']
-    console.group('Personajes de Marvel');
-    marvel.forEach(superheroe => {
-        console.log(superheroe);
-    });
-    console.groupEnd();
+    const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+    const contenedorBotones = document.getElementById('recordatorios');
+
+   function renderizarBotones() {
+        const boton = document.createElement('button');
+        const texto = document.createElement('p')
+        contenedorBotones.appendChild(boton);
+        contenedorBotones.appendChild(texto);
+   }
+
+   dias.forEach(renderizarBotones)
+
 }); 
